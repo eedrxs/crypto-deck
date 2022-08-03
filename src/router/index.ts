e82a4d467fdbc30404c976cd306623f0b19a0ea1
eedrxs <alabialade@gmail.com>
2022-08-03 09:21:10 +0100

    Renamed HomePage component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 
-import HomePage from "../views/Homepage.vue"
+import HomePage from "../views/HomePage.vue"
 import Dashboard from "../views/Dashboard.vue"
 
 const routes = [
